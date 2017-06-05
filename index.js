e55766a03d462724ba935fc58438602e77d7f72a
AkshayaKL <achu4008@yahoo.com>
2017-06-05 13:40:56 +0530

    changes

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var count=0;
+var counter=0;
 function clickzz() {
+      document.getElementById("audio").play();
+       counter=0;
+       swap();
+    
     
     document.getElementById("pa").className='unhidden'; 
     var randomX1 = Math.ceil(Math.random() * 10);
@@ -15,7 +20,7 @@ function clickzz() {
     if(randomX2 === randomX3)
    { count++;
 
-        var t = "JackPOTTT!!!";
+        var t = "JackPOt!!!";
       document.getElementById("score").innerHTML=count;
         }
     }
@@ -26,5 +31,25 @@ function clickzz() {
         document.getElementById("chance").innerHTML = t;
     }
     document.getElementById('pa').innerHTML = t;
+
 };
+
+function swap()
+{
+
+var t=Math.floor(Math.random() * 10);
+var x=Math.floor(Math.random() * 10);
+var j=Math.floor(Math.random() * 10);
+document.getElementById("randomX1").innerHTML = t;
+document.getElementById("randomX2").innerHTML = x;
+document.getElementById("randomX1").innerHTML = j;
+
+if(counter<=20)
+{requestAnimationFrame(swap);
+counter++;
+
+}
+
+
+}
 
